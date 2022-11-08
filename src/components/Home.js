938ce705b1f1563964d34804a63d832ddec75ace
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-11-09 01:26:36 +0300

    TA-7 add home page component

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+import Header from '../layout/Header.js'
+import SideBar from '../layout/SideBar.js'
+import TaskOption from './TaskOption.js'
+import TaskContainer from './TaskContainer.js'
+import TaskLayout from './TaskLayout.js'
+import TaskCard from './TaskCard.js'
+import Root from '../layout/Root.js'
+import Container from '../layout/Container.js'
+
+
+export default function Home() {
+
+    return (
+        <>
+            <Header />
+            <Root>
+                <SideBar />
+                <Container>
+                    <TaskOption />
+                    <TaskContainer>
+                        <TaskLayout itemComponent={TaskCard} />
+                    </TaskContainer>
+                </Container>
+            </Root>
+        </>
+    );
+}
