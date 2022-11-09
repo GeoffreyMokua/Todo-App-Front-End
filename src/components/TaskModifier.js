bc43d1cdb0982c4c2dd46bd59effe53f8274bd0c
fliblip <wickynjy@gmail.com>
2022-11-09 10:10:44 +0300

    TA-12 toggle modal onOpen

--- src/components/TaskModifier.js
+++ src/components/TaskModifier.js
@@ -12,14 +12,38 @@ export default function TaskModifier({todo, show, handleClose}) {
     };
 
     const toggleStatus = (status) => {
-      fetch(`http://localhost:9292/api/todo/status/${todo.id}`), {
+      fetch(`http://localhost:9292/api/todo/status/${todo.id}`, {
         method: "PATCH",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({
           status: status,
+        }),
+      })
+        .then((response) => response.json())
+        .then((record) => {
+          console.log(record);
         })
-      }
+        .catch((err) => {
+          console.error(err)
+        });
+
+        toggleOpen()
     }
+
+    const modalOnOpen = () =>{
+      fetch(`http://localhost:9292/api/todo/${todo.id}`)
+          .then((res) => res.json())
+          .then((todo_obj) => {
+              if(todo_obj.category){
+                  setCategory(todo_obj.category.category)
+              } else{
+                  setCategory('')
+              }
+          })
+          .catch((err) => {
+              console.error(err);
+          });
+  }
     
   return (
     <Modal
