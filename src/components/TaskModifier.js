6b2e2ef5341306ee821fbe8e2c8ab14ccad258ba
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-09 13:17:01 +0300

    made changes  to modal component

--- src/components/TaskModifier.js
+++ src/components/TaskModifier.js
@@ -1,76 +1,75 @@
-import React, { useState } from 'react'
 import Modal from 'react-bootstrap/Modal'
+import { useState } from 'react'
 
 export default function TaskModifier({todo, show, handleClose}) {
-
-    const[dropdown, setDropdown] = useState(false);
+    
+    const [dropdown, setDropdown] = useState(false);
     const toggleOpen = () => setDropdown(!dropdown);
 
     const [category, setCategory] = useState('')
     const handleTaskModifier = () => {
-      handleClose(false);
-    };
-
-    const toggleStatus = (status) => {
-      fetch(`http://localhost:9292/api/todo/status/${todo.id}`, {
-        method: "PATCH",
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-          status: status,
-        }),
-      })
-        .then((response) => response.json())
-        .then((record) => {
-          console.log(record);
+            handleClose(false);
+        };
+    const toggleStatus = (stauts) => {
+        fetch(`http://localhost:9292/api/todo/status/${todo.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                status: stauts,
+            }),
         })
-        .catch((err) => {
-          console.error(err)
-        });
-
+            .then((response) => response.json())
+            .then((record) => {
+                console.log(record);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+        
         toggleOpen()
     }
-
-    const modalOnOpen = () =>{
-      fetch(`http://localhost:9292/api/todo/${todo.id}`)
-          .then((res) => res.json())
-          .then((todo_obj) => {
-              if(todo_obj.category){
-                  setCategory(todo_obj.category.category)
-              } else{
-                  setCategory('')
-              }
-          })
-          .catch((err) => {
-              console.error(err);
-          });
-  }
-
-  const [title, setTitle] = useState(todo.title);
-  const [content, setContent] = useState(todo.content);
-
-  const handleForm = () =>{
-    todo.title = title
-    todo.content = content
-    fetch(`http://localhost:9292/api/todo/${todo.id}`, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            title: title,
-            content: content,
-        }),
-    })
-        .then((response) => response.json())
-        .then((record) => {
-            console.log(record);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
-        handleTaskModifier();
-}
     
-  return (
-    <Modal
+    const modalOnOpen = () =>{
+        fetch(`http://localhost:9292/api/todo/${todo.id}`)
+            .then((res) => res.json())
+            .then((todo_obj) => {
+                if(todo_obj.category){
+                    setCategory(todo_obj.category.category)
+                } else{
+                    setCategory('')
+                }
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+
+    const [title, setTitle] = useState(todo.title);
+    const [content, setContent] = useState(todo.content);
+    
+    const handleForm = () =>{
+        todo.title = title
+        todo.content = content
+        fetch(`http://localhost:9292/api/todo/${todo.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                title: title,
+                content: content,
+            }),
+        })
+            .then((response) => response.json())
+            .then((record) => {
+                console.log(record);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+            handleTaskModifier();
+    }
+
+    return (
+        <Modal
             show={show}
             onHide={handleTaskModifier}
             onShow={modalOnOpen}
@@ -197,5 +196,5 @@ export default function TaskModifier({todo, show, handleClose}) {
                 </div>
             </div>
         </Modal>
-  )
+    );
 }
