1f5f13c5359b36da748aafe72df66ce02f075543
fliblip <wickynjy@gmail.com>
2022-11-09 10:17:22 +0300

    TA-12 Created the task option component, declare context

---
+++ src/components/TaskOption.js
@@ -0,0 +1,11 @@
+import React, { useContext } from 'react'
+
+export default function TaskOption() {
+
+    const currentOption = useContext(OptionContext);
+    const {show, handleOpen, handleClose} = useContext(ModalContext);
+    
+  return (
+    
+  )
+}
