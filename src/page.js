dd82d921341aa0842859c1159b71a57c77a88698
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-09 11:31:44 +0300

    added the functionality of the page

---
+++ src/page.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Page.css'
+
+export default function Page() {
+    return (
+            <div className="container-fluid m-0 p-0">
+                <div className="row m-0 p-0">
+                    <div className="header col-12">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, mollitia eaque omnis nam pariatur assumenda. Error delectus animi porro alias nesciunt adipisci ex iusto! Harum optio ut quibusdam cum aperiam!
+                        </p>
+                    </div>
+                    <div className="container-fluid m-0 p-0">
+                        <div className='row'>
+                            <div className='sidebar col-xs-12 col-sm-12 col-md-2 col-lg-2 col-xl-2'>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil harum aspernatur eum cupiditate, placeat deserunt sapiente repellat? Eligendi ullam dolore quos! Officiis ut praesentium sed? Perferendis placeat totam at odio!
+                                </p>
+                            </div>
+                            <div className='container col-xs-12 col-sm-12 col-md-10 col-xl-10'>
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam voluptates iste tempora molestiae laborum sapiente nesciunt reiciendis incidunt, placeat debitis atque. Sint esse voluptates facilis dicta dolorem porro! Omnis, libero?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
