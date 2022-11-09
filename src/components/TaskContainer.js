c9caae8f9cf5a22c268a5dd3fa00b2ae4ae99c8b
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-11-09 22:07:25 +0300

    rectify spelling mistakes

--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -1,13 +1,15 @@
-import { useContext } from "react";
 import { endPointContext } from "../services/OptionProvider.js";
 
-function TaskContiner({ children }) {
+function TaskContainer({children}) {
+
     const url = useContext(endPointContext);
-    return url ? (
+
+    return url ?(
         <div className='task-container container-fluid d-flex justify-content-center flex-wrap py-2 px-2'>
             {children}
         </div>
     )
-        : <></>
+    : <></>
 };
-export default TaskContiner;
+
+export default TaskContainer;
