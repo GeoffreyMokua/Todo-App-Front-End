17bc4a7f75e501bfecafe59e8ce6bc1fb835fc5f
fliblip <wickynjy@gmail.com>
2022-11-09 10:21:13 +0300

    TA-13 return modal

--- src/components/TaskOption.js
+++ src/components/TaskOption.js
@@ -13,8 +13,40 @@ export default function TaskOption() {
         const catModalClose = () => {
             setCatModalStatus(false);
         };
-        
+
   return (
-    
+    <div className="task-options container-fluid d-flex justify-content-between px-5">
+            <div className="fs-4 fw-bold text-secondary text-capitalize">
+                {currentOption}
+            </div>
+            <div>
+                <button
+                    type="button"
+                    className="btn btn-outline-dark text-success"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        handleOpen();
+                    }}
+                >
+                    <i className="fas  fa-plus"></i> &nbsp; Task
+                </button>
+                &nbsp;
+                <button
+                    type="button"
+                    className="btn btn-outline-dark text-success"
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        catModalOpen();
+                    }}
+                >
+                    <i className="fas fa-plus"></i> &nbsp; Category
+                </button>
+            </div>
+            <TaskModal status={show} handleClose={handleClose} />
+            <CategoryModal
+                status={catModalStatus}
+                handleClose={catModalClose}
+            />
+        </div>  
   )
 }
