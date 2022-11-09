29879bf4b7f240da432ed6d0647a3ea72685695b
fliblip <wickynjy@gmail.com>
2022-11-09 10:13:44 +0300

    TA-12 return card on submit form

--- src/components/TaskModifier.js
+++ src/components/TaskModifier.js
@@ -71,9 +71,131 @@ export default function TaskModifier({todo, show, handleClose}) {
     
   return (
     <Modal
-        show={show}
-        onHide={handleTaskModifier}
-        onShow={modalOnOpen}
-    
+            show={show}
+            onHide={handleTaskModifier}
+            onShow={modalOnOpen}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <div className="card">
+                <div className="card-header">Update Todo</div>
+                <div className="card-body">
+                    <form>
+                        <div className="mb-3">
+                            <label className="form-label">Title</label>
+                            <input
+                                name="title"
+                                type="title"
+                                className="form-control"
+                                placeholder="Title"
+                                value={title}
+                                onChange={(se) => {
+                                    setTitle(se.target.value);
+                                }}
+                            />
+                        </div>
+                        <div className="  mb-3">
+                            <div>
+                                <label className="form-label">Category</label>
+                                <fieldset className="row mx-0 justify-content-between">
+                                    {/* d-flex justify-content-between */}
+                                    <div className="col-10 px-0">
+                                        <input
+                                            // disabled
+                                            id="disabledTextInput"
+                                            name="category"
+                                            type="text"
+                                            className="form-control align-self-stretch"
+                                            placeholder="Disabled"
+                                            value={category}
+                                            onChange={() => {
+                                                return null;
+                                            }}
+                                        />
+                                    </div>
+                                    <div className="btn-group col-auto px-0">
+                                        <button
+                                            type="button"
+                                            className="btn btn-outline-dark text-success dropdown-toggle"
+                                            data-toggle="dropdown"
+                                            aria-haspopup="true"
+                                            aria-expanded="false"
+                                            onClick={toggleOpen}
+                                        >
+                                            Status
+                                        </button>
+                                        <div
+                                            className={`dropdown-menu ${
+                                                dropdown ? "show" : ""
+                                            }`}
+                                        >
+                                            <a
+                                                className="dropdown-item"
+                                                href="#/"
+                                                onClick={(e) => {
+                                                    e.preventDefault()
+                                                    e.stopPropagation();
+                                                    toggleStatus("In Progress");
+                                                }}
+                                            >
+                                                In Progress
+                                            </a>
+                                            <div className="dropdown-divider"></div>
+                                            <a
+                                                className="dropdown-item"
+                                                href="#/"
+                                                onClick={(e) => {
+                                                    e.preventDefault();
+                                                    e.stopPropagation();
+                                                    toggleStatus("Done");
+                                                }}
+                                            >
+                                                Completed
+                                            </a>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                            </div>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Todo Content</label>
+                            <textarea
+                                name="content"
+                                className="form-control"
+                                rows="3"
+                                placeholder="Content"
+                                value={content}
+                                onChange={(se) => {
+                                    setContent(se.target.value);
+                                }}
+                            ></textarea>
+                        </div>
+                        <button
+                            type="submit"
+                            className="btn btn-primary mb-3"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                                handleForm();
+                            }}
+                        >
+                            Save
+                        </button>
+                        &nbsp;
+                        <button
+                            type="button"
+                            className="btn btn-primary mb-3"
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                handleTaskModifier();
+                            }}
+                        >
+                            close
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </Modal>
   )
 }
