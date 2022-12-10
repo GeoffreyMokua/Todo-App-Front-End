3b4c6a6e5e9f9cdba73e080aa6f5f93f00014614
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-12-10 22:45:19 +0300

    header changes

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -2,7 +2,7 @@ import '../css/App.css'
 export default function Header() {
     return (
         <nav className="navbar navbar-expand-lg navbar-primary bg-light shadow-sm">
-            <a className="navbar-brand mx-5" href="#"><h2 className='text-success'>Todo App</h2></a>
+            <a className="navbar-brand mx-5" href="/#"><h2 className='text-success'>Todo App</h2></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -10,15 +10,15 @@ export default function Header() {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav m-auto">
                 <li className="nav-item active">
-                    <a className="nav-link text-success" href="#">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-link text-success" href="/#">Home <span className="sr-only">(current)</span></a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link text-success" href="#">About</a>
+                    <a className="nav-link text-success" href="/#">About</a>
                 </li>
                 
 
                 <li className="nav-item">
-                    <a className="nav-link disabled" href="#">User Profile</a>
+                    <a className="nav-link disabled" href="/#">User Profile</a>
                 </li>
                 </ul>
 
